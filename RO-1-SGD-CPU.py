5087ac5d43595060e72f4653d448b867effa0c2f
devNowRO <68102723+devNowRO@users.noreply.github.com>
2024-05-07 10:48:45 +0500

    Update RO-1-SGD-CPU.py

--- RO-1-SGD-CPU.py
+++ RO-1-SGD-CPU.py
@@ -1,3 +1,6 @@
+# Code written as part of NFRP-22-41-55 (1st March 2023 - 30 April 2024)
+# =======================================================================
+
 from matplotlib import pyplot as py
 from sklearn.model_selection import train_test_split
 import sklearn
